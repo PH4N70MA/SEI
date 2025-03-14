d41738444693e8b6e3eaf7e64b2c2a323537f974
Alexandru Railean <alexandru.railean@mib.utm.md>
2025-03-14 12:03:01 +0200

    set pins

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,16 +57,42 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define VCP_TX_Pin GPIO_PIN_2
-#define VCP_TX_GPIO_Port GPIOA
+#define KEYPAD_ROW_0_Pin GPIO_PIN_0
+#define KEYPAD_ROW_0_GPIO_Port GPIOA
+#define KEYPAD_ROW_1_Pin GPIO_PIN_1
+#define KEYPAD_ROW_1_GPIO_Port GPIOA
+#define KEYPAD_ROW_2_Pin GPIO_PIN_2
+#define KEYPAD_ROW_2_GPIO_Port GPIOA
+#define KEYPAD_ROW_3_Pin GPIO_PIN_3
+#define KEYPAD_ROW_3_GPIO_Port GPIOA
+#define KEYPAD_COLUM_0_Pin GPIO_PIN_4
+#define KEYPAD_COLUM_0_GPIO_Port GPIOA
+#define KEYPAD_COLUM_1_Pin GPIO_PIN_5
+#define KEYPAD_COLUM_1_GPIO_Port GPIOA
+#define KEYPAD_COLUM_2_Pin GPIO_PIN_6
+#define KEYPAD_COLUM_2_GPIO_Port GPIOA
+#define KEYPAD_COLUM_3_Pin GPIO_PIN_7
+#define KEYPAD_COLUM_3_GPIO_Port GPIOA
+#define LCD_D6_Pin GPIO_PIN_0
+#define LCD_D6_GPIO_Port GPIOB
+#define LCD_EN_Pin GPIO_PIN_1
+#define LCD_EN_GPIO_Port GPIOB
+#define LCD_RS_Pin GPIO_PIN_8
+#define LCD_RS_GPIO_Port GPIOA
+#define LCD_D7_Pin GPIO_PIN_12
+#define LCD_D7_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
-#define VCP_RX_Pin GPIO_PIN_15
-#define VCP_RX_GPIO_Port GPIOA
-#define LD3_Pin GPIO_PIN_3
-#define LD3_GPIO_Port GPIOB
+#define LED_RED_Pin GPIO_PIN_3
+#define LED_RED_GPIO_Port GPIOB
+#define LED_GREEN_Pin GPIO_PIN_4
+#define LED_GREEN_GPIO_Port GPIOB
+#define LCD_D4_Pin GPIO_PIN_6
+#define LCD_D4_GPIO_Port GPIOB
+#define LCD_D5_Pin GPIO_PIN_7
+#define LCD_D5_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
