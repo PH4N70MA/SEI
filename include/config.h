6548ca7f96375b6ecbef6ab6070908cf9f336c2d
Alexandru Railean <alexandru.railean@mib.utm.md>
2025-05-21 03:19:56 +0300

    crearea circuitului de simulare

--- include/config.h
+++ include/config.h
@@ -3,6 +3,14 @@
 
 #define LED_PIN 13
 
+#define GREEN_LIGHT_VERTICAL 11
+#define ORANGE_LIGHT_VERTICAL 12
+#define RED_LIGHT_VERTICAL 13
+
+#define GREEN_LIGHT_HORIZONTAL 8
+#define ORANGE_LIGHT_HORIZONTAL 9
+#define RED_LIGHT_HORIZONTAL 10
+
 #define BUTTON_PIN 2
 
 // Define the baud rate for the serial communication
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,48 @@
 
 void setup(void) 
 {
-  setUp();
+
+  pinMode(GREEN_LIGHT_VERTICAL, OUTPUT);
+  pinMode(ORANGE_LIGHT_VERTICAL, OUTPUT);
+  pinMode(RED_LIGHT_VERTICAL, OUTPUT);
+
+  pinMode(GREEN_LIGHT_HORIZONTAL, OUTPUT);
+  pinMode(ORANGE_LIGHT_HORIZONTAL, OUTPUT);
+  pinMode(RED_LIGHT_HORIZONTAL, OUTPUT);
+
+  digitalWrite(GREEN_LIGHT_VERTICAL, LOW);
+  digitalWrite(ORANGE_LIGHT_VERTICAL, LOW);
+  digitalWrite(RED_LIGHT_VERTICAL, LOW);
+
+  digitalWrite(GREEN_LIGHT_HORIZONTAL, LOW);
+  digitalWrite(ORANGE_LIGHT_HORIZONTAL, LOW);
+  digitalWrite(RED_LIGHT_HORIZONTAL, LOW);
 }
 
 void loop(void) 
 {
-  buttonPressTask();
+  digitalWrite(GREEN_LIGHT_VERTICAL, HIGH);
+  delay(500);
+  digitalWrite(GREEN_LIGHT_VERTICAL, LOW);
+
+  digitalWrite(ORANGE_LIGHT_VERTICAL, HIGH);
+  delay(500);
+  digitalWrite(ORANGE_LIGHT_VERTICAL, LOW);
+
+  digitalWrite(RED_LIGHT_VERTICAL, HIGH);
+  delay(500);
+  digitalWrite(RED_LIGHT_VERTICAL, LOW);
+
+  digitalWrite(GREEN_LIGHT_HORIZONTAL, HIGH);
+  delay(500);
+  digitalWrite(GREEN_LIGHT_HORIZONTAL, LOW);
+
+  digitalWrite(ORANGE_LIGHT_HORIZONTAL, HIGH);
+  delay(500);
+  digitalWrite(ORANGE_LIGHT_HORIZONTAL, LOW);
+
+  digitalWrite(RED_LIGHT_HORIZONTAL, HIGH);
+  delay(500);
+  digitalWrite(RED_LIGHT_HORIZONTAL, LOW);
+  // buttonPressTask();
 }
